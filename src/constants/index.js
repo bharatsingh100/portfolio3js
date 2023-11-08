3c6da26a51d50ad58de8cbf766c4a3258ad39035
unknown <bharatsinghkushwah1000@gmail.com>
2023-11-08 22:46:07 +0530

    constant file change

--- src/constants/index.js
+++ src/constants/index.js
@@ -123,7 +123,7 @@ import {
       iconBg: "#383E56",
       date: "July 2022 - Present",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Developing and maintaining web applications using Java, Spring Boot and React.js",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
         "Implementing responsive design and ensuring cross-browser compatibility.",
         "Participating in code reviews and providing constructive feedback to other developers.",
